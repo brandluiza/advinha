ced70bd72ba9b4fcfc3603fee179b73bb3788d83
brandluiza <karlaluizaalvessilva@gmail.com>
2025-03-07 22:39:30 -0300

    Add files via upload

---
+++ advinha.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine(" Oque é, o que é... ");
+Thread.Sleep(2000);
+Console.WriteLine(" que tem coroas e escamas, mas nao é peixe? ");
+Thread.Sleep(3000);
+Console.WriteLine(" O abacaxi" );
